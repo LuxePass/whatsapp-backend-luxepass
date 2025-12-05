bf0ebc77b3306f99c809a6ba5e4dd2aefe2fd140
devapus <folorunsopraise12@gmail.com>
2025-12-05 09:22:06 +0100

    done

--- src/config/database.js
+++ src/config/database.js
@@ -2,30 +2,44 @@ import mongoose from "mongoose";
 import logger from "./logger.js";
 import config from "./env.js";
 
+let cached = global.mongoose;
+
+if (!cached) {
+	cached = global.mongoose = { conn: null, promise: null };
+}
+
 /**
- * Connect to MongoDB
+ * Connect to MongoDB (optimized for Vercel serverless)
  */
 export async function connectDB() {
-	try {
-		const conn = await mongoose.connect(config.database.uri, {
-			maxPoolSize: 10,
-			serverSelectionTimeoutMS: 30000,
-			connectTimeoutMS: 30000,
-		});
-
-		logger.info(`MongoDB Connected: ${conn.connection.host}`);
-
-		mongoose.connection.on("error", (err) => {
-			logger.error(`MongoDB connection error: ${err}`);
-		});
-
-		mongoose.connection.on("disconnected", () => {
-			logger.warn("MongoDB disconnected");
-		});
-
-		return conn;
-	} catch (error) {
-		logger.error(`Error connecting to MongoDB: ${error.message}`);
-		process.exit(1);
+	if (cached.conn) {
+		return cached.conn;
 	}
+
+	if (!cached.promise) {
+		logger.info("Connecting to MongoDB...");
+
+		cached.promise = mongoose
+			.connect(config.database.uri, {
+				maxPoolSize: 10,
+				serverSelectionTimeoutMS: 30000,
+				connectTimeoutMS: 30000,
+			})
+			.then((mongoose) => {
+				logger.info(`MongoDB Connected: ${mongoose.connection.host}`);
+
+				mongoose.connection.on("error", (err) => {
+					logger.error(`MongoDB connection error: ${err}`);
+				});
+
+				mongoose.connection.on("disconnected", () => {
+					logger.warn("MongoDB disconnected");
+				});
+
+				return mongoose;
+			});
+	}
+
+	cached.conn = await cached.promise;
+	return cached.conn;
 }
